e8fa1e62977fc6ad109136bc02e0f753b8f7581a
Ibad Ullah Kahttana <ibadullahkahttana@gmail.com>
2022-07-20 10:55:15 +0500

    Mster Commit
    Commit 35

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://192.168.100.17:8000/api';
+export const BASE_URL = 'http://test.proptechglobal.net/api';
 
 // http://test.proptechglobal.net/api
+// http://192.168.100.17:8000/api
